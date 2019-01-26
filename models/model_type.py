fe239e9be6a57690efb316768e3f66476fdec2c0
Waris Quamer <mr.warisquamer@gmail.com>
2019-01-26 14:15:10 +0530

    Update model_type.py

--- models/model_type.py
+++ models/model_type.py
@@ -9,13 +9,15 @@ class ModelType(Enum):
     multihead = 0,
     rnn = 1,
     cnn = 2,
-    bcann = 3
+    bcann = 3,
+    bcsann = 4
 
 
 MODELS = {
     ModelType.cnn.name: CnnSiameseNet,
     ModelType.rnn.name: LSTMBasedSiameseNet,
     ModelType.multihead.name: MultiheadAttentionSiameseNet,
-    ModelType.bcann.name: AttentionCnn
+    ModelType.bcann.name: AttentionCnn,
+    ModelType.bcsann.name:AttentionSCnn
 }
 

5b0cc5f179f00849447e4c868f05f04011a63b97
Waris Quamer <mr.warisquamer@gmail.com>
2019-01-28 15:09:21 +0530

    Update run.py

--- run.py
+++ run.py
@@ -149,7 +149,7 @@ def main():
                         help='pipeline mode')
 
     parser.add_argument('model',
-                        choices=['rnn', 'cnn', 'multihead', 'bcann', 'bcsann'],
+                        choices=['rnn', 'cnn', 'multihead', 'bcann', 'bcsann', 'bcsannwmh'],
                         help='model to be used')
 
     parser.add_argument('dataset',

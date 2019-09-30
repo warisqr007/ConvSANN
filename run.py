7c9c7ff830785b06accea357e65cbf19dbda5b82
Waris Quamer <mr.warisquamer@gmail.com>
2019-09-30 17:33:05 +0530

    Update run.py

--- run.py
+++ run.py
@@ -207,7 +207,7 @@ def main():
 
     mode = args.mode
     
-    kb_dict_f = '../../data/sequence_and_features/pair_features.pkl'
+    kb_dict_f = 'data/sequence_and_features/pair_features.pkl'
     
     if 'train' in mode:
         train(main_config, model_config, args.model, args.dataset, kb_dict_f)
